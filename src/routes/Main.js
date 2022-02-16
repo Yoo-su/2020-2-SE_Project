d55aba3645aacbd184a5a1c141ab86831f7d4c9e
yoosu <rhan0871@naver.com>
2022-02-16 18:28:59 +0900

    메인 페이지 수정

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,22 +1,24 @@
 import React from "react";
 import mainImg from "../imgs/chefHat.png";
-import mainBack from "../imgs/mainBack.jpg";
+import mainBack from "../imgs/mainBack2.jpg";
 import "./Main.css";
 
 function Main() {
   return (
-    <div id="main">
+    <div id="mainPage">
       <div
         id="mainContent"
         style={{ backgroundImage: `url(${mainBack})`, backgroundSize: "cover" }}
       >
         <div id="insideContent">
-          <img id="mainImg" src={mainImg} alt={mainImg}></img>
-          <br></br>
-          <br></br>
-          <b style={{ fontSize: "80px", fontWeight: "lighter" }}>
-            에브리 레스토랑
-          </b>
+          <img id="descImg" src={mainImg} alt={mainImg}></img>
+
+          <div id="serviceDesc">
+          <label>
+            주문, 쿠킹알림, 직원관리 및 매상집계 자동화 시스템
+          </label>
+          <b>에브리 레스토랑</b>
+          </div>
         </div>
       </div>
     </div>
